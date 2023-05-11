63d0c71deb6fa1d9fac9b0b1429153e9efb22088
trungln <trungln14@ghtk.co>
2023-05-11 21:51:07 +0700

    fix tạm conflict

--- Drink Tracker/ViewControlers/UpdateUserInfo/UpdateUserInfoVCViewController.swift
+++ Drink Tracker/ViewControlers/UpdateUserInfo/UpdateUserInfoVCViewController.swift
@@ -7,16 +7,13 @@ class UpdateUserInfoVCViewController: UIViewController {
     
     @IBOutlet private weak var waterLabel: UILabel!
     @IBOutlet private weak var heightTextField: UITextField!
-    @IBOutlet private weak var wakeUpTimeLabel: UILabel!
-    @IBOutlet private weak var sleepTimeLabel: UILabel!
+//    @IBOutlet private weak var wakeUpTimeLabel: UILabel!
+//    @IBOutlet private weak var sleepTimeLabel: UILabel!
     @IBOutlet private weak var weightTextField: UITextField!
-
     @IBOutlet weak var progresView: UIProgressView!
-    
     @IBOutlet weak var loadingText: UILabel!
     @IBOutlet weak var calculateView: UIView!
     @IBOutlet weak var resultView: UIStackView!
-    
     @IBOutlet weak var viewFemale: UIView!
     @IBOutlet weak var viewMale: UIView!
     @IBOutlet weak var imageFemale: UIView!
@@ -47,63 +44,10 @@ class UpdateUserInfoVCViewController: UIViewController {
             self.selectSex(isMale: false)
         }
         .disposed(by: disposeBag)
-        wakeUpTimeLabel.rx.tapGesture().when(.recognized)
-            .subscribe(onNext: { [weak self] _ in
-                guard let self = self else { return }
-                let vnTime = UserInfo.shared.timeWakeUp.convertTo(region: VNRegion)
-                let pickTimeVC = PickTimeVC.newVC(selectedHour: vnTime.hour, selectedMinute: vnTime.minute, title: "Chọn thời gian thức dậy")
-                pickTimeVC.onConfirmChange = { [weak self] hour, minute in
-                    guard let date = DateInRegion(components: {
-                        $0.hour = hour
-                        $0.minute = minute
-                    }, region: VNRegion)?.date,
-                          let self = self
-                    else { return }
-                    UserInfo.shared.timeWakeUp = date
-                    UserInfo.shared.saveToUserDefault()
-                    self.wakeUpTimeLabel.textColor = UIColor.black
-                    self.wakeUpTimeLabel.text = UserInfo.shared.timeWakeUp.convertTo(region: VNRegion).toFormat("HH:mm")
-                }
-                let vc = BottomSheetVC.newVC(contentVC: pickTimeVC, contentHeight: 320)
-                vc.modalPresentationStyle = .overFullScreen
-                self.present(vc, animated: true)
-            })
-            .disposed(by: disposeBag)
-                
-        sleepTimeLabel.rx.tapGesture().when(.recognized)
-            .subscribe(onNext: { [weak self] _ in
-                guard let self = self else { return }
-                let vnTime = UserInfo.shared.timeGoToSleep.convertTo(region: VNRegion)
-                let pickTimeVC = PickTimeVC.newVC(selectedHour: vnTime.hour, selectedMinute: vnTime.minute, title: "Chọn thời gian đi ngủ")
-                pickTimeVC.onConfirmChange = { [weak self] hour, minute in
-                    guard let date = DateInRegion(components: {
-                        $0.hour = hour
-                        $0.minute = minute
-                    }, region: VNRegion)?.date,
-                          let self = self
-                    else { return }
-                    UserInfo.shared.timeGoToSleep = date
-                    UserInfo.shared.saveToUserDefault()
-                    self.sleepTimeLabel.textColor = UIColor.black
-                    self.sleepTimeLabel.text = UserInfo.shared.timeGoToSleep.convertTo(region: VNRegion).toFormat("HH:mm")
-                }
-                let vc = BottomSheetVC.newVC(contentVC: pickTimeVC, contentHeight: 320)
-                vc.modalPresentationStyle = .overFullScreen
-                self.present(vc, animated: true)
-            })
-            .disposed(by: disposeBag)
+        
     }
     
-    @IBAction func onUpdateBtnPress(_ sender: Any) {
-        guard let heightStr = heightTextField.text,
-              let height = Double(heightStr),
-              let weightStr = weightTextField.text,
-              let weight = Double(weightStr)
-        else { return }
-        UserInfo.shared.weight = weight
-        UserInfo.shared.height = height
-        UserInfo.shared.saveToUserDefault()
-        
+    func bindRx(){
         heightTextField.rx.tapGesture().when(.recognized).subscribe { [weak self] _ in
             guard let self = self else { return }
             let editWeightVC = PickTimeVC()
@@ -139,7 +83,63 @@ class UpdateUserInfoVCViewController: UIViewController {
             self.present(vc, animated: true)
         }
         .disposed(by: disposeBag)
+    // MARK: - TODO: sửa lại trên xib trước
+//        wakeUpTimeLabel.rx.tapGesture().when(.recognized)
+//            .subscribe(onNext: { [weak self] _ in
+//                guard let self = self else { return }
+//                let vnTime = UserInfo.shared.timeWakeUp.convertTo(region: VNRegion)
+//                let pickTimeVC = PickTimeVC.newVC(selectedHour: vnTime.hour, selectedMinute: vnTime.minute, title: "Chọn thời gian thức dậy")
+//                pickTimeVC.onConfirmChange = { [weak self] hour, minute in
+//                    guard let date = DateInRegion(components: {
+//                        $0.hour = hour
+//                        $0.minute = minute
+//                    }, region: VNRegion)?.date,
+//                          let self = self
+//                    else { return }
+//                    UserInfo.shared.timeWakeUp = date
+//                    UserInfo.shared.saveToUserDefault()
+//                    self.wakeUpTimeLabel.textColor = UIColor.black
+//                    self.wakeUpTimeLabel.text = UserInfo.shared.timeWakeUp.convertTo(region: VNRegion).toFormat("HH:mm")
+//                }
+//                let vc = BottomSheetVC.newVC(contentVC: pickTimeVC, contentHeight: 320)
+//                vc.modalPresentationStyle = .overFullScreen
+//                self.present(vc, animated: true)
+//            })
+//            .disposed(by: disposeBag)
+//
+//        sleepTimeLabel.rx.tapGesture().when(.recognized)
+//            .subscribe(onNext: { [weak self] _ in
+//                guard let self = self else { return }
+//                let vnTime = UserInfo.shared.timeGoToSleep.convertTo(region: VNRegion)
+//                let pickTimeVC = PickTimeVC.newVC(selectedHour: vnTime.hour, selectedMinute: vnTime.minute, title: "Chọn thời gian đi ngủ")
+//                pickTimeVC.onConfirmChange = { [weak self] hour, minute in
+//                    guard let date = DateInRegion(components: {
+//                        $0.hour = hour
+//                        $0.minute = minute
+//                    }, region: VNRegion)?.date,
+//                          let self = self
+//                    else { return }
+//                    UserInfo.shared.timeGoToSleep = date
+//                    UserInfo.shared.saveToUserDefault()
+//                    self.sleepTimeLabel.textColor = UIColor.black
+//                    self.sleepTimeLabel.text = UserInfo.shared.timeGoToSleep.convertTo(region: VNRegion).toFormat("HH:mm")
+//                }
+//                let vc = BottomSheetVC.newVC(contentVC: pickTimeVC, contentHeight: 320)
+//                vc.modalPresentationStyle = .overFullScreen
+//                self.present(vc, animated: true)
+//            })
+//            .disposed(by: disposeBag)
+    }
     
+    @IBAction func onUpdateBtnPress(_ sender: Any) {
+        guard let heightStr = heightTextField.text,
+              let height = Double(heightStr),
+              let weightStr = weightTextField.text,
+              let weight = Double(weightStr)
+        else { return }
+        UserInfo.shared.weight = weight
+        UserInfo.shared.height = height
+        UserInfo.shared.saveToUserDefault()
     }
     
     func selectSex(isMale: Bool = true) {
@@ -149,26 +149,6 @@ class UpdateUserInfoVCViewController: UIViewController {
         self.imageFemale.backgroundColor = isMale ? .lightGray : UIColor.appColor
         self.viewMale.alpha = isMale ? 1 : 0.6
         self.viewFemale.alpha = isMale ? 0.6 : 1
-    }
-    
-    @IBAction func onUpdateBtnPress(_ sender: Any) {
-//        guard let heightStr = heightTextField.text,
-//              let height = Double(heightStr),
-//              let weightStr = weightTextField.text,
-//              let weight = Double(weightStr)
-//        else { return }
-//        UserInfo.shared.weight = weight
-//        UserInfo.shared.height = height
-//        UserInfo.shared.saveToUserDefault()
-//
-//        Setting.shared.drinkTarget = weight * 30
-//        Setting.shared.saveToUserDefault()
-//
-//        AppConfig.shared.isOnboard = true
-//        AppConfig.shared.saveToUserDefault()
-//
-//        AppCoordinator.shared.goToMainVC()
-        updateWaterNeedAmount()
     }
     
     @IBAction func onHeightChange(_ sender: Any) {
