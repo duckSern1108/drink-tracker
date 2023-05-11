94960fbdd80acddea92cb0b7e559ef7df530373e
Sern Duck <>
2023-05-11 20:28:52 +0700

    update updateInfoVC

--- Drink Tracker/ViewControlers/UpdateUserInfo/UpdateUserInfoVCViewController.swift
+++ Drink Tracker/ViewControlers/UpdateUserInfo/UpdateUserInfoVCViewController.swift
@@ -1,12 +1,69 @@
 import UIKit
+import SwiftDate
+import RxSwift
+import RxGesture
 
 class UpdateUserInfoVCViewController: UIViewController {
-
+    
     @IBOutlet private weak var waterLabel: UILabel!
     @IBOutlet private weak var heightTextField: UITextField!
+    @IBOutlet private weak var wakeUpTimeLabel: UILabel!
+    @IBOutlet private weak var sleepTimeLabel: UILabel!
     @IBOutlet private weak var weightTextField: UITextField!
     @IBOutlet private weak var nameTextField: UITextField!
-        
+    
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        wakeUpTimeLabel.rx.tapGesture().when(.recognized)
+            .subscribe(onNext: { [weak self] _ in
+                guard let self = self else { return }
+                let vnTime = UserInfo.shared.timeWakeUp.convertTo(region: VNRegion)
+                let pickTimeVC = PickTimeVC.newVC(selectedHour: vnTime.hour, selectedMinute: vnTime.minute, title: "Chọn thời gian thức dậy")
+                pickTimeVC.onConfirmChange = { [weak self] hour, minute in
+                    guard let date = DateInRegion(components: {
+                        $0.hour = hour
+                        $0.minute = minute
+                    }, region: VNRegion)?.date,
+                          let self = self
+                    else { return }
+                    UserInfo.shared.timeWakeUp = date
+                    UserInfo.shared.saveToUserDefault()
+                    self.wakeUpTimeLabel.textColor = UIColor.black
+                    self.wakeUpTimeLabel.text = UserInfo.shared.timeWakeUp.convertTo(region: VNRegion).toFormat("HH:mm")
+                }
+                let vc = BottomSheetVC.newVC(contentVC: pickTimeVC, contentHeight: 320)
+                vc.modalPresentationStyle = .overFullScreen
+                self.present(vc, animated: true)
+            })
+            .disposed(by: disposeBag)
+                
+        sleepTimeLabel.rx.tapGesture().when(.recognized)
+            .subscribe(onNext: { [weak self] _ in
+                guard let self = self else { return }
+                let vnTime = UserInfo.shared.timeGoToSleep.convertTo(region: VNRegion)
+                let pickTimeVC = PickTimeVC.newVC(selectedHour: vnTime.hour, selectedMinute: vnTime.minute, title: "Chọn thời gian đi ngủ")
+                pickTimeVC.onConfirmChange = { [weak self] hour, minute in
+                    guard let date = DateInRegion(components: {
+                        $0.hour = hour
+                        $0.minute = minute
+                    }, region: VNRegion)?.date,
+                          let self = self
+                    else { return }
+                    UserInfo.shared.timeGoToSleep = date
+                    UserInfo.shared.saveToUserDefault()
+                    self.sleepTimeLabel.textColor = UIColor.black
+                    self.sleepTimeLabel.text = UserInfo.shared.timeGoToSleep.convertTo(region: VNRegion).toFormat("HH:mm")
+                }
+                let vc = BottomSheetVC.newVC(contentVC: pickTimeVC, contentHeight: 320)
+                vc.modalPresentationStyle = .overFullScreen
+                self.present(vc, animated: true)
+            })
+            .disposed(by: disposeBag)
+    }
+    
     @IBAction func onUpdateBtnPress(_ sender: Any) {
         guard let heightStr = heightTextField.text,
               let height = Double(heightStr),
